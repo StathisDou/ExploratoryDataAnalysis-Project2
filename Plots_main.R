a89634334163d5b13ba62596cd47a29bf74b5b34
Efstathios Dou <doustathis@gmail.com>
2022-09-18 00:18:54 +0300

    Plot4 Commit (script & png)

--- Plots_main.R
+++ Plots_main.R
@@ -1,8 +1,11 @@
 
 NEI<-0
 SCC<-0
+MERGEDDATA<-0
 
 bufferdata<-function(){
       NEI <<- readRDS("summarySCC_PM25.rds")
       SCC <<- readRDS("Source_Classification_Code.rds")
+      MERGEDDATA <<- merge(NEI, SCC, by = "SCC")
+      library(ggplot2)
 }
---
+++ plot4.R
@@ -0,0 +1,15 @@
+
+CreatePlot4<-function(){
+      
+      #get indices where (Short name) contains the word coal from SCC
+      containscoalID <- grep("coal", MERGEDDATA$Short.Name, ignore.case = TRUE)
+      containscoal <- MERGEDDATA[containscoalID,]
+      containscoal <- aggregate(Emissions ~ year, containscoal, sum) 
+      #Select device PNG
+      png(filename = "plot4.png", width = 640, height = 480, units = "px",bg = "white")
+      par(mfrow = c(1,1), mar = c(4,4,2,2))
+      plot4p <- ggplot(containscoal, aes(year, Emissions)) + geom_point() + geom_line() + ggtitle("Summary of Emissions from Coal Combustion related emissions") + xlab("Year") + ylab("Total PM2.5 Emissions") 
+      print(plot4p)
+      dev.off()
+      
+}
